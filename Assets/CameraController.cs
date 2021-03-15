4a60240a7d1fb6798d108330d76ec87f4381be9b
Gabriel Bouchard <gaboutch@outlook.com>
2021-03-15 11:02:49 -0400

    working but has weird visual glitch

---
+++ Assets/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float sensitivity = 0.5f;
+    [SerializeField] private float speed = 5;
+    private Camera camera;
+    private float currentPitch = 0;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+        }
+
+        Rotate();
+        Move();
+    }
+
+    void Rotate()
+    {
+        Vector2 change = new Vector2(
+            0.1f * Input.GetAxis("Mouse X") * camera.fieldOfView * sensitivity,
+            0.1f * Input.GetAxis("Mouse Y") * camera.fieldOfView * sensitivity);
+        transform.Rotate(new Vector3(currentPitch, 0, 0));
+        transform.Rotate(new Vector3(0, change.x, 0));
+        currentPitch += change.y;
+        currentPitch = Mathf.Clamp(currentPitch, -90, 90);
+        transform.Rotate(new Vector3(-currentPitch, 0, 0));
+    }
+
+    void Move()
+    {
+        Vector3 translation = new Vector3(0, 0, 0);
+        translation.z += Input.GetKey(KeyCode.W) ? 1 : 0;
+        translation.z -= Input.GetKey(KeyCode.S) ? 1 : 0;
+        translation.y += Input.GetKey(KeyCode.Space) ? 1 : 0;
+        translation.y -= Input.GetKey(KeyCode.LeftControl) ? 1 : 0;
+        translation.x += Input.GetKey(KeyCode.D) ? 1 : 0;
+        translation.x -= Input.GetKey(KeyCode.A) ? 1 : 0;
+        transform.Translate(translation * (speed * Time.deltaTime));
+    }
+}
--- Assets/VoxelRenderer.cs
+++ Assets/VoxelRenderer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,51 +7,61 @@ using UnityEngine.UI;
 public class VoxelRenderer : MonoBehaviour
 {
     [SerializeField] private ComputeShader shader;
-    [SerializeField] private float maxDistance=500f;
-    [SerializeField] private float voxelSize=1f;
+    [SerializeField] private float maxDistance = 500f;
+    [SerializeField] private float voxelSize = 1f;
+    [SerializeField] private Color skyColor = Color.gray;
+    [SerializeField] Color[] materialColors;
+
     private RenderTexture texture;
     private RawImage image;
     private VoxelData voxelData;
     private ComputeBuffer voxelDataBuffer;
+    private ComputeBuffer materialColorsBuffer;
 
     private void Start()
     {
         image = GetComponent<RawImage>();
         CreateRenderTexture(Screen.width, Screen.height);
         voxelDataBuffer = new ComputeBuffer(VoxelData.SIZE_X * VoxelData.SIZE_Y * VoxelData.SIZE_Z, sizeof(int));
+        materialColorsBuffer = new ComputeBuffer(materialColors.Length, sizeof(float) * 4);
         CreateVoxelData();
-
         image.texture = texture;
         SetShaderConstants();
     }
+
     void Update()
     {
-        if (Screen.width!=texture.width||Screen.height != texture.height)
+        if (Screen.width != texture.width || Screen.height != texture.height)
         {
-            CreateRenderTexture(Screen.width,Screen.height);
+            CreateRenderTexture(Screen.width, Screen.height);
         }
+
         RenderVoxelDataInComputeShader();
     }
 
     private void OnDisable()
     {
         voxelDataBuffer.Dispose();
+        materialColorsBuffer.Dispose();
     }
 
     void CreateVoxelData()
     {
         voxelData = new VoxelData();
+        int count = 0;
         for (int x = 0; x < VoxelData.SIZE_X; x++)
         {
             for (int y = 0; y < VoxelData.SIZE_Y; y++)
             {
                 for (int z = 0; z < VoxelData.SIZE_Z; z++)
                 {
-                    voxelData[x, y, z] = 1;
+                    count++;
+                    if (Vector3.Distance(new Vector3( VoxelData.SIZE_X / 2, VoxelData.SIZE_X / 2, VoxelData.SIZE_X / 2),new Vector3(x,y,z)) < VoxelData.SIZE_X / 2)
+                        voxelData[x, y, z] = count % (materialColors.Length);
+                    else voxelData[x, y, z] = -1;
                 }
             }
         }
-
     }
 
     void CreateRenderTexture(int width, int height)
@@ -58,29 +69,31 @@ public class VoxelRenderer : MonoBehaviour
         texture = new RenderTexture(width, height, 1);
         texture.enableRandomWrite = true;
         texture.Create();
-        shader.SetInts("TextureResolution", new int[]{texture.width,texture.height});
+        shader.SetInts("TextureResolution", new int[] {texture.width, texture.height});
     }
 
     void SetShaderConstants()
     {
         int id = shader.FindKernel("CSMain");
-        shader.SetInts("VoxelDataResolution",new int[]{VoxelData.SIZE_X, VoxelData.SIZE_Y, VoxelData.SIZE_Z});
+        shader.SetInts("VoxelDataResolution", new int[] {VoxelData.SIZE_X, VoxelData.SIZE_Y, VoxelData.SIZE_Z});
         shader.SetFloat("MaxDistance", maxDistance);
         shader.SetFloat("VoxelSize", voxelSize);
         shader.SetTexture(id, "OutputTexture", texture);
+        shader.SetFloats("SkyColor", skyColor.r, skyColor.g, skyColor.b);
+        shader.SetBuffer(id, "VoxelMaterialColor", materialColorsBuffer);
+        materialColorsBuffer.SetData(materialColors);
     }
-    
+
     void RenderVoxelDataInComputeShader()
     {
         voxelDataBuffer.SetData(voxelData.Data);
-        
-        Matrix4x4 viewRotation = Matrix4x4.Inverse(Matrix4x4.Rotate(Camera.main.transform.rotation));
-        Vector3 viewPosition=Camera.main.transform.position;
+
+        Matrix4x4 viewRotation = Matrix4x4.Rotate(Camera.main.transform.rotation);
+        Vector3 viewPosition = Camera.main.transform.position;
         int id = shader.FindKernel("CSMain");
-        SetShaderConstants();
         shader.SetBuffer(id, "VoxelData", voxelDataBuffer);
         shader.SetMatrix("ViewRotation", viewRotation);
         shader.SetVector("ViewPosition", viewPosition);
-        shader.Dispatch(id, (int)Mathf.Ceil(texture.width / 8f), (int)Mathf.Ceil(texture.height / 8f), 1);
+        shader.Dispatch(id, (int) Mathf.Ceil(texture.width / 8f), (int) Mathf.Ceil(texture.height / 8f), 1);
     }
 }
